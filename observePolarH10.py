005141268ec709f16c99cf9db4724814feb2e48f
JoergSchultz-TWT <Joerg.Schultz@Tier-Wohl-Team.de>
2023-07-22 18:25:31 +0200

    writing data to files

--- observePolarH10.py
+++ observePolarH10.py
@@ -1,5 +1,7 @@
 import asyncio
 import sys
+import time
+
 import aioconsole
 from bleak import BleakClient
 
@@ -8,21 +10,28 @@ from Sensors.Polar import PolarH10
 ADDRESS = "C9:C4:09:A0:49:1D"
 
 
+hr_file = open("data/hr_data.csv", "w")
+hrv_file = open("data/hrv_data.csv", "w")
+acc_file = open("data/acc_data.csv", "w")
+ecg_file = open("data/ecg_data.csv", "w")
+
+
 def handle_hr(heart_rate):
-    print(f"Heart Rate: {heart_rate}")
+    hr_file.write(f"{time.time()},{heart_rate}\n")
 
 
 def handle_hrv(hrv):
     print(f"HR Variability: {hrv}")
+    hrv_file.write(f"{time.time()},{hrv}\n")
 
 
 def handle_acc(x, y, z):
-    pass
-    # print(f"ACC X: {x}  Y: {y}  Z: {z}")
+    acc_file.write(f"{time.time()},{x},{y},{z}\n")
 
 
 def handle_ecg(ecg):
     print(f"ECG: {ecg}")
+    ecg_file.write(f"{time.time()},{ecg}\n")
 
 
 async def run(client):
@@ -34,6 +43,10 @@ async def run(client):
     await aioconsole.ainput('Running: Press a key to quit\n')
     print("Stopping Polar data...", flush=True)
     await polar.disconnect()
+    hr_file.close()
+    hrv_file.close()
+    acc_file.close()
+    ecg_file.close()
     print("[CLOSED] application closed.", flush=True)
     sys.exit(0)
 
